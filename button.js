0f85ff27e1d1a1de7c212a13b931e1d95d1ad531
Leandro Lucas <leoviolinist22@gmail.com>
2024-09-27 10:54:51 -0300

    removing js file

--- button.js
+++
@@ -1,9 +0,0 @@
-
-
-var header = document.getElementsByClassName("box-button")[0]
-var btns = header.getElementsByClassName("button-on-off")
-for(var i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", function(){
-        this.className += " active"
-    })
-}

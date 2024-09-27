5496bbd4ca7048251f80154e8d9cc9ad232b4755
Leandro Lucas <leoviolinist22@gmail.com>
2024-09-27 10:09:10 -0300

    adding initial code

---
+++ button.js
@@ -0,0 +1,10 @@
+
+/*
+var header = document.getElementsByClassName("box-button")[0]
+var btns = header.getElementsByClassName("button-on-off")
+for(var i = 0; i < btns.length; i++){
+    btns[i].addEventListener("click", function(){
+        this.className += " active"
+    })
+}
+*/

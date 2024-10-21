76e6e73374e94bb3dba11377e48959d139ebb24e
Leandro Lucas <leoviolinist22@gmail.com>
2024-10-21 14:49:38 -0300

    retirando a funcionalidade do php

---
+++ button.js
@@ -0,0 +1,6 @@
+
+const buttonSubmit = document.getElementById("button-submit");
+function changePage(){
+    window.location.href = "index.html";
+}
+buttonSubmit.addEventListener("click", changePage)
